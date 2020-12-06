9a7e2216ed2a60addae4e2bda73956a90e550a38
AnimeKato <o0katchan0o@gmail.com>
2020-12-06 11:56:19 +0000

    Set the pgt command to guild only

--- commands/si.js
+++ commands/si.js
@@ -1,5 +1,6 @@
 module.exports = {
 	name: 'si',
+    guildOnly: true
 	description: 'server info',
 	execute(message) {
 		message.channel.send(`Server name: ${message.guild.name}\nTotal members: ${message.guild.memberCount}`);}
