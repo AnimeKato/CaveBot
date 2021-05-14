f40006f65170a3dfe3507991aa52a7602686a86b
AnimeKato <o0katchan0o@tuta.io>
2021-05-14 08:44:00 +0000

    The bot is finally fixed and working.

--- commands/si.js
+++ commands/si.js
@@ -2,7 +2,7 @@ const discord = require("discord.js");
 module.exports = {
 	name: 'si',
     	guildOnly: true,
-  description: "Print server information"
+  description: "Print server information",
   aliases: ['serverinfo'],
 execute(message) {
 		message.channel.send(`Server name: ${message.guild.name}\nTotal members: ${message.guild.memberCount}`);}
